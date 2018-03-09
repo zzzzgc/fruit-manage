a5d2e9d3ed2b48d2f117462313e5a29ee1587e09
partnerccz <2779017796@qq.com>
2018-03-09 12:21:30 +0800

    添加商户的编辑功能

--- src/main/java/com/fruit/manage/controller/customer/CustomerController.java
+++ src/main/java/com/fruit/manage/controller/customer/CustomerController.java
@@ -8,7 +8,7 @@ import com.jfinal.aop.Before;
 import com.jfinal.plugin.activerecord.tx.Tx;
 import org.apache.log4j.Logger;
 
-import java.util.Date;
+import java.util.*;
 
 public class CustomerController extends BaseController{
     private Logger logger = Logger.getLogger(getClass());
@@ -16,21 +16,42 @@ public class CustomerController extends BaseController{
     public void save() {
         BusinessAuth businessAuth=getModel(BusinessAuth.class,"",true);
         BusinessInfo businessInfo=getModel(BusinessInfo.class,"",true);
-        // 给店铺认证赋值
-        businessAuth.setCreateTime(new Date());
-        businessAuth.setUpdateTime(new Date());
-        businessAuth.setAudit(2);
-        // TODO 获取用户ID并赋值
-        businessAuth.setUId(1);
-        // 给店铺信息
-        businessInfo.setUId(1);
-        businessInfo.setCreateTime(new Date());
-        businessInfo.setUpdateTime(new Date());
-        businessInfo.save(); //添加商户信息
-        businessAuth.save(); //添加商户认证
+        if(businessInfo.getId()!=null && businessInfo.getId()>0){
+            businessAuth.setUpdateTime(new Date());
+            businessInfo.setUpdateTime(new Date());
+            businessAuth.update();
+            businessInfo.update();
+        }else{
+            // 给店铺认证赋值
+            businessAuth.setCreateTime(new Date());
+            businessAuth.setUpdateTime(new Date());
+            businessAuth.setAudit(2);
+            // TODO 获取用户ID并赋值
+            businessAuth.setUId(1);
+            // 给店铺信息
+            businessInfo.setUId(1);
+            businessInfo.setCreateTime(new Date());
+            businessInfo.setUpdateTime(new Date());
+            businessInfo.save(); //添加商户信息
+            businessAuth.save(); //添加商户认证
+        }
     }
 
     public void getData(){
         System.out.println("getData");
+    }
+
+    /**
+     * 根据编号ID获取商户信息和商户认证
+     */
+    public void info(){
+        Integer id =getParaToInt("id");
+        if (id==null || id<=0){
+            renderErrorText("商品ID不能为空");
+        }
+        Map map =new HashMap();
+        map.put("businessInfo",BusinessInfo.dao.getByID(id));
+        map.put("businessAuth",BusinessAuth.dao.getBusinessAuthByBusinessInfoID(id));
+        renderJson(map);
     }
 }
--- src/main/java/com/fruit/manage/model/BusinessAuth.java
+++ src/main/java/com/fruit/manage/model/BusinessAuth.java
@@ -8,4 +8,19 @@ import com.fruit.manage.model.base.BaseBusinessAuth;
 @SuppressWarnings("serial")
 public class BusinessAuth extends BaseBusinessAuth<BusinessAuth> {
 	public static final BusinessAuth dao = new BusinessAuth().dao();
+
+    /**
+     * 根据商户信息的ID获取商户认证的信息
+     * @param businessInfoID
+     * @return
+     */
+	public BusinessAuth getBusinessAuthByBusinessInfoID(Integer businessInfoID){
+		StringBuilder sql=new StringBuilder();
+		sql.append("select ba.id,ba.legal_person_name,ba.identity,ba.bank_account, ");
+		sql.append("ba.business_license,ba.auth_type,ba.img_identity_front, ");
+		sql.append("ba.img_identity_reverse,ba.img_license,ba.img_online_shop ");
+		sql.append("from b_business_auth ba ");
+		sql.append("where ba.u_id = (select bi.u_id from b_business_info bi where bi.id=?) ");
+		return findFirst(sql.toString(),businessInfoID);
+	}
 }
