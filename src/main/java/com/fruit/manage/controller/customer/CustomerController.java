28c8c58adcf0330e08cc3543b5483051daa3a980
partnerccz <2779017796@qq.com>
2018-03-09 14:12:23 +0800

    解决版本冲突

--- src/main/java/com/fruit/manage/controller/customer/CustomerController.java
+++ src/main/java/com/fruit/manage/controller/customer/CustomerController.java
@@ -3,7 +3,8 @@ package com.fruit.manage.controller.customer;
 import com.fruit.manage.base.BaseController;
 import com.fruit.manage.model.BusinessAuth;
 import com.fruit.manage.model.BusinessInfo;
-import com.fruit.manage.util.Constant;
+import com.fruit.manage.model.BusinessUser;
+import com.fruit.manage.model.User;
 import com.jfinal.aop.Before;
 import com.jfinal.plugin.activerecord.tx.Tx;
 import org.apache.log4j.Logger;
@@ -16,6 +17,7 @@ public class CustomerController extends BaseController{
     public void save() {
         BusinessAuth businessAuth=getModel(BusinessAuth.class,"",true);
         BusinessInfo businessInfo=getModel(BusinessInfo.class,"",true);
+        Integer saleUserId =getParaToInt("saleUserId");
         if(businessInfo.getId()!=null && businessInfo.getId()>0){
             businessAuth.setUpdateTime(new Date());
             businessInfo.setUpdateTime(new Date());
@@ -35,6 +37,7 @@ public class CustomerController extends BaseController{
             businessInfo.save(); //添加商户信息
             businessAuth.save(); //添加商户认证
         }
+        BusinessUser.dao.updateBusinessUserSaleIDByUid(1, saleUserId);
     }
 
 
@@ -42,12 +45,20 @@ public class CustomerController extends BaseController{
      * 获取商户数据a
      */
     public void getData(){
+
         int pageNum = getParaToInt("pageNum", 1);
         int pageSize = getParaToInt("pageSize", 10);
 
+        String searchProvince = getPara("search_province");
+        String searchCity = getPara("search_city");
+        String salesName = getPara("sales_name");
+        String business_name = getPara("business_name");
+        String[] create_time = getParaValues("format_create_time");
+
         String orderBy = getPara("prop");
-        boolean isASC = "ascending".equals(getPara("order"));// ascending为升序，其他为降序
-        renderJson(BusinessInfo.dao.getData(pageNum, pageSize, orderBy, isASC));
+        // ascending为升序，其他为降序
+        boolean isASC = "ascending".equals(getPara("order"));
+        renderJson(BusinessInfo.dao.getData(searchProvince,searchCity,salesName,business_name,create_time,pageNum, pageSize, orderBy, isASC));
     }
 
     /**
@@ -62,5 +73,12 @@ public class CustomerController extends BaseController{
         map.put("businessInfo",BusinessInfo.dao.getByID(id));
         map.put("businessAuth",BusinessAuth.dao.getBusinessAuthByBusinessInfoID(id));
         renderJson(map);
+    }
+
+    /**
+     * 获取所有的销售人员信息
+     */
+    public void getAllSaleUser(){
+        renderJson(User.dao.getAllUserByRoleKey());
     }
 }
