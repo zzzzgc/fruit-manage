36047fa000e0151685f88aad2c5825bd26cd8f82
partnerccz <2779017796@qq.com>
2018-03-21 16:16:49 +0800

    删掉多余的表

--- src/main/java/com/fruit/manage/model/_MappingKit.java
+++ src/main/java/com/fruit/manage/model/_MappingKit.java
@@ -47,7 +47,6 @@ public class _MappingKit {
 		arp.addMapping("b_product_type", "id", ProductType.class);
 		arp.addMapping("b_type", "id", Type.class);
 		arp.addMapping("b_type_group", "id", TypeGroup.class);
-		arp.addMapping("newtable", "id", Newtable.class);
 	}
 }
 
