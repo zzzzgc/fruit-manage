1bcab045a5fdbb178ebd7f6482806f4eeefcdc3f
partnerccz <2779017796@qq.com>
2018-03-21 13:10:11 +0800

    规范控制器名

--- src/main/java/com/fruit/manage/controller/Procurement/ProcurementPlanController.java
+++
@@ -1,8 +0,0 @@
-package com.fruit.manage.controller.Procurement;
-
-import com.fruit.manage.base.BaseController;
-import org.apache.log4j.Logger;
-
-public class ProcurementPlanController extends BaseController {
-    public static Logger logger= Logger.getLogger(ProcurementPlanController.class);
-}
