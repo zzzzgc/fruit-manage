90e35a7ba218c34dd71da4c82aa0148b92d3f4db
zzzzgc <1098766713@qq.com>
2018-03-22 16:12:08 +0800

    修改所有商品的修改 添加 删除操作的日志记录和处理

--- src/main/java/com/fruit/manage/model/base/BaseProcurementPlan.java
+++ src/main/java/com/fruit/manage/model/base/BaseProcurementPlan.java
@@ -9,12 +9,12 @@ import com.jfinal.plugin.activerecord.IBean;
 @SuppressWarnings("serial")
 public abstract class BaseProcurementPlan<M extends BaseProcurementPlan<M>> extends Model<M> implements IBean {
 
-	public void setId(java.lang.Integer id) {
+	public void setId(java.lang.String id) {
 		set("id", id);
 	}
 
-	public java.lang.Integer getId() {
-		return getInt("id");
+	public java.lang.String getId() {
+		return getStr("id");
 	}
 
 	public void setProcurementId(java.lang.Integer procurementId) {
