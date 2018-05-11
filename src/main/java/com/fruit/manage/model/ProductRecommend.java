bd0773e3527ec6c0a8a51e26d13708657c2a4a9c
partnerccz <2779017796@qq.com>
2018-05-11 14:00:12 +0800

    解决新增商品可不选择标签

--- src/main/java/com/fruit/manage/model/ProductRecommend.java
+++ src/main/java/com/fruit/manage/model/ProductRecommend.java
@@ -14,32 +14,35 @@ import java.util.List;
  */
 @SuppressWarnings("serial")
 public class ProductRecommend extends BaseProductRecommend<ProductRecommend> {
-	public static final ProductRecommend dao = new ProductRecommend().dao();
+    public static final ProductRecommend dao = new ProductRecommend().dao();
 
-	public int[] getProductRecommend(int productId) {
-		List<ProductRecommend> list = find("select type from b_product_recommend where product_id=?", productId);
-		int[] recommends = new int[list.size()];
-		for(int i = 0; i < list.size(); i++) {
-			recommends[i] = list.get(i).getType();
-		}
-		return recommends;
-	}
+    public int[] getProductRecommend(int productId) {
+        List<ProductRecommend> list = find("select type from b_product_recommend where product_id=?", productId);
+        int[] recommends = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            recommends[i] = list.get(i).getType();
+        }
+        return recommends;
+    }
 
-	public boolean saveProductRecommend(boolean delete, int productId, Integer[] recommends) {
-		return Db.tx(new IAtom() {
-			@Override
-			public boolean run() throws SQLException {
-				if(delete) {
-					Db.update("delete from b_product_recommend where product_id=?", productId);
-				}
-				List<Object[]> params = new ArrayList<>();
-				for(Integer recommend : recommends) {
-					params.add(new Object[]{productId, recommend});
-				}
-				String sql = "insert into b_product_recommend(product_id, type, create_time) values(?,?,now())";
-				Db.batch(sql, Common.listTo2Array(params), params.size());
-				return true;
-			}
-		});
-	}
+    public boolean saveProductRecommend(boolean delete, int productId, Integer[] recommends) {
+        return Db.tx(new IAtom() {
+            @Override
+            public boolean run() throws SQLException {
+                if (delete) {
+                    Db.update("delete from b_product_recommend where product_id=?", productId);
+                }
+                List<Object[]> params = new ArrayList<>();
+                if (recommends != null && recommends.length > 0) {
+
+                    for (Integer recommend : recommends) {
+                        params.add(new Object[]{productId, recommend});
+                    }
+                    String sql = "insert into b_product_recommend(product_id, type, create_time) values(?,?,now())";
+                    Db.batch(sql, Common.listTo2Array(params), params.size());
+                }
+                return true;
+            }
+        });
+    }
 }
