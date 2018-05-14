79b68859ba638b61a2f1d489d0c1ddeb9c12bd06
partnerccz <2779017796@qq.com>
2018-05-14 18:48:28 +0800

    根据需求解决入库时间可以动态选择的问题

--- src/main/java/com/fruit/manage/controller/warehouse/put/WarehousePutController.java
+++ src/main/java/com/fruit/manage/controller/warehouse/put/WarehousePutController.java
@@ -29,7 +29,7 @@ public class WarehousePutController extends BaseController {
     /**
      * 获取根据条件查询出来的数据集合
      */
-    public void getAllPutWarehouseInfo(){
+    public void getAllPutWarehouseInfo() {
         int pageNum = getParaToInt("pageNum", 1);
         int pageSize = getParaToInt("pageSize", 10);
         Map map = new HashMap();
@@ -37,8 +37,8 @@ public class WarehousePutController extends BaseController {
         // ascending为升序，其他为降序
         boolean isASC = "ascending".equals(getPara("order"));
         String[] create_time = getParaValues("format_create_time");
-        map.put("createTime",create_time);
-        renderJson(PutWarehouse.dao.getAllInfo(pageNum,pageSize,orderBy,isASC,map));
+        map.put("createTime", create_time);
+        renderJson(PutWarehouse.dao.getAllInfo(pageNum, pageSize, orderBy, isASC, map));
     }
 
     /**
@@ -46,15 +46,18 @@ public class WarehousePutController extends BaseController {
      */
     public void createWarehouse() {
         try {
-        PutWarehouse putWarehouse = new PutWarehouse();
-        putWarehouse.setPutNum(0);
-        putWarehouse.setPutTypeNum(0);
-        putWarehouse.setPutTotalPrice(new BigDecimal(0));
-        putWarehouse.setPutType(0);
-        putWarehouse.setWarehouseAddress("默认地址");
-        putWarehouse.setCreateTime(new Date());
-        putWarehouse.save();
-        renderNull();
+            System.out.println("---------------putInTime----------------");
+            String putIntTimeStr = getPara("putInTime");
+            System.out.println("putInTime:"+putIntTimeStr);
+            PutWarehouse putWarehouse = new PutWarehouse();
+            putWarehouse.setPutNum(0);
+            putWarehouse.setPutTypeNum(0);
+            putWarehouse.setPutTotalPrice(new BigDecimal(0));
+            putWarehouse.setPutType(0);
+            putWarehouse.setWarehouseAddress("默认地址");
+            putWarehouse.setCreateTime(DateAndStringFormat.strToDate(putIntTimeStr,"yyyy-MM-dd HH:mm:ss"));
+            putWarehouse.save();
+            renderNull();
         } catch (Exception e) {
             renderErrorText("创建失败!");
         }
@@ -64,15 +67,15 @@ public class WarehousePutController extends BaseController {
      * 根据入库编号删除入库单
      */
     @Before(Tx.class)
-    public void delWarehouse(){
-        try{
-            Integer putId= getParaToInt("putId");
+    public void delWarehouse() {
+        try {
+            Integer putId = getParaToInt("putId");
             // 根据入库单编号先删除关联的入库详细信息
             PutWarehouseDetail.dao.delWarehousePutDetailByWPId(putId);
             // 根据入库单编号删除入库信息
             PutWarehouse.dao.deleteById(putId);
             renderNull();
-        }catch (Exception e){
+        } catch (Exception e) {
             renderErrorText("删除失败!");
         }
     }
@@ -80,17 +83,17 @@ public class WarehousePutController extends BaseController {
     /**
      * 导出入库
      */
-    public void exportWarehousePut(){
-        Integer putId=getParaToInt("putId");
-        List<PutWarehouseDetail> detailList=PutWarehouseDetail.dao.getAllInfoByPutId(putId);
-        if(detailList!=null && detailList.size()>0){
+    public void exportWarehousePut() {
+        Integer putId = getParaToInt("putId");
+        List<PutWarehouseDetail> detailList = PutWarehouseDetail.dao.getAllInfoByPutId(putId);
+        if (detailList != null && detailList.size() > 0) {
             List<String[]> lists = new ArrayList<>();
             for (int i = 0; i < detailList.size(); i++) {
                 // 修改了长度
                 String[] strs = new String[9];
                 strs[0] = detailList.get(i).getProductName();
                 strs[1] = detailList.get(i).getProductStandardName();
-                strs[2] = detailList.get(i).getProductStandardId()+"";
+                strs[2] = detailList.get(i).getProductStandardId() + "";
                 strs[3] = detailList.get(i).getProductWeight() + "";
                 strs[4] = detailList.get(i).getProcurementPrice() + "";
                 strs[5] = detailList.get(i).getProcurementTotalPrice() + "";
@@ -99,18 +102,18 @@ public class WarehousePutController extends BaseController {
                 strs[8] = detailList.get(i).getProcurementName();
                 lists.add(strs);
             }
-            java.lang.String fileName = UUID.randomUUID().toString().replaceAll("-", "")+".xlsx";
-            String path =CommonController.FILE_PATH;
-            String title ="入库单";
+            java.lang.String fileName = UUID.randomUUID().toString().replaceAll("-", "") + ".xlsx";
+            String path = CommonController.FILE_PATH;
+            String title = "入库单";
             Integer uid = getSessionAttr(Constant.SESSION_UID);
             // 获取当前操作用户
             User user = User.dao.findById(uid);
-            String [] headers={"商品名","规格名","规格编码","重量(斤)","采购单价","采购总价","摊位费","入库数量","采购人"};
+            String[] headers = {"商品名", "规格名", "规格编码", "重量(斤)", "采购单价", "采购总价", "摊位费", "入库数量", "采购人"};
             Map map = new HashMap();
             map.put("path", path);
             map.put("fileName", fileName);
             map.put("title", title);
-            map.put("createBy",user.getName());
+            map.put("createBy", user.getName());
             map.put("header", headers);
             map.put("listData", lists);
             try {
@@ -121,7 +124,7 @@ public class WarehousePutController extends BaseController {
             } catch (ExcelException e) {
                 renderErrorText("导出Excel异常!");
             }
-        }else {
+        } else {
             renderErrorText("该入库单没有记录!");
         }
     }
